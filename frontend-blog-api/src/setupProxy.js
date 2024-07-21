26131fa672616ea7272bcf23633f807f625751a6
Veikko M. <veikkosakari01@gmail.com>
2024-07-21 14:54:59 +0300

    modified proxy url

--- frontend-blog-api/src/setupProxy.js
+++ frontend-blog-api/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function proxyFunction(app) {
-	const target = import.meta.env.BACKEND_URL || "localhost:5000";
+	const target = import.meta.env.VITE_BACKEND_URL || "localhost:5000";
 	app.use(
 		"/",
 		createProxyMiddleware({
