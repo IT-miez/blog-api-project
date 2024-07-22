d97b934d4d1beac91a69fd66dba0a7ca655275c8
Veikko M. <veikkosakari01@gmail.com>
2024-07-22 16:14:50 +0300

    new edit

--- frontend-blog-api/src/App.jsx
+++ frontend-blog-api/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import ShortPost from "./components/ShortPost";
 
+// Testing imports
+
 function App() {
 	const [posts, setPosts] = useState([]);
 
