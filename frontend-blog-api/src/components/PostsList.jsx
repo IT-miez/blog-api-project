f5ad1a1b591c402c13b7b8cb19c398a5baaf0fe7
Veikko M. <veikkosakari01@gmail.com>
2025-02-20 16:38:24 +0200

    added skeleton loader instead of plaintext when loading posts from backend

--- frontend-blog-api/src/components/PostsList.jsx
+++ frontend-blog-api/src/components/PostsList.jsx
@@ -1,16 +1,31 @@
-
 import ShortPost from "./ShortPost";
-
 import useQuery from "../hooks/useQuery";
-
-// Testing imports
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export function PostsList() {
-	const { data, loading, error, refetch} = useQuery("/post/get")
-    
+	const { data, loading, error, refetch } = useQuery("/post/get");
 
 	if (loading) {
-		return <div><p>Loading posts...</p></div>;
+		return (
+			<div className="shortpost-outer-wrapper">
+				{[...Array(3)].map((_, index) => (
+					<div key={index} className="shortpost-master">
+						<div className="shortpost-wrapper">
+							<div className="shortpost-left">
+								<div className="shortpost-left-inner">
+									<p className="title-text"><Skeleton height={28} width={200} /></p> {/* Simulating title */}
+									<p className="author-text"><Skeleton height={18} width={150} /></p> {/* Simulating author */}
+								</div>
+							</div>
+							<div className="shortpost-right">
+								<p className="date-text"><Skeleton height={18} width={100} /></p> {/* Simulating date */}
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		);
 	}
 
 	if (!data?.posts) {
@@ -18,19 +33,18 @@ export function PostsList() {
 	}
 
 	return (
-			<div className="shortpost-outer-wrapper">
-				{data.posts.map((item) => (
-						<ShortPost
-							key={crypto.randomUUID()}
-							postId={item._id}
-							author={item.author.username}
-							creationDate={item.createdAtFormatted}
-							title={item.title}
-						/>
-					))}
-			</div>
+		<div className="shortpost-outer-wrapper">
+			{data.posts.map((item) => (
+				<ShortPost
+					key={item._id} // Using _id instead of crypto.randomUUID() for consistency
+					postId={item._id}
+					author={item.author.username}
+					creationDate={item.createdAtFormatted}
+					title={item.title}
+				/>
+			))}
+		</div>
 	);
 }
 
-
-export default PostsList
+export default PostsList;
