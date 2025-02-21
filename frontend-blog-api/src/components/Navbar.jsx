f7878e1b57c2c99a18246c70c8d90c2c8512aef4
Veikko M. <veikkosakari01@gmail.com>
2025-02-21 18:11:33 +0200

    fixed mobile navbar view and image view on posts

--- frontend-blog-api/src/components/Navbar.jsx
+++ frontend-blog-api/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import '../index.css';
 import '../styles/navbar.css';
 import { Link } from 'react-router-dom';
-
 import parseJwt from '../utils/parseJwt';
 
 export const Navbar = () => {
@@ -10,39 +9,43 @@ export const Navbar = () => {
         window.location.href = '/';
     }
 
-    let button1 = <button />;
-    let button2 = <button />;
-    let button3 = '';
-    let createPostButton = '';
-    let information = '';
-    let profileButton = '';
-
     const authToken = localStorage.getItem('auth_token');
+
+    let authButtons = null;
+    let userButtons = null;
+    let createPostButton = null;
+    let information = null;
+
     if (!authToken) {
-        button1 = (
-            <Link to="/login">
-                <button className="login-button">Log In</button>
-            </Link>
-        );
-        button2 = (
-            <Link to="/register">
-                <button className="register-button">Register</button>
-            </Link>
+        authButtons = (
+            <div className="buttons">
+                <Link to="/login">
+                    <button className="login-button">Log In</button>
+                </Link>
+                <Link to="/register">
+                    <button className="register-button">Register</button>
+                </Link>
+            </div>
         );
     } else {
         const tokenInformation = parseJwt(authToken);
-        button1 = <button onClick={logoutButton}>Logout</button>;
-        button2 = '';
+
         information = (
             <div>
                 <h2 className="username-hello">Hello,</h2>
                 <p className="username-box">{tokenInformation.username}</p>
             </div>
         );
-        profileButton = (
-            <Link to={`/profile/${tokenInformation.id}`}>
-                <button className="profile-button">Profile</button>
-            </Link>
+
+        userButtons = (
+            <div className="buttons">
+                <Link to={`/profile/${tokenInformation.id}`}>
+                    <button className="profile-button">Profile</button>
+                </Link>
+                <button onClick={logoutButton} className="logout-button">
+                    Logout
+                </button>
+            </div>
         );
 
         createPostButton = (
@@ -54,39 +57,26 @@ export const Navbar = () => {
         );
     }
 
-    if (!profileButton) {
-        button3 = (
-            <Link to="/">
-                <button id="blogsite-button" className="blogsite-button">
-                    Blogsite
-                </button>
-            </Link>
-        );
-    } else {
-        button3 = (
-            <Link to="/">
-                <button id="blogsite-button" className="blogsite-button">
-                    Blogsite
-                </button>
-            </Link>
-        );
-    }
-
     return (
         <div>
             <div className="navbar">
                 <div className="container">
                     <div className="buttons-left">
                         <div className="buttons buttons-left">
-                            {button3}
+                            <Link to="/">
+                                <button
+                                    id="blogsite-button"
+                                    className="blogsite-button">
+                                    Blogsite
+                                </button>
+                            </Link>
                             {information}
                         </div>
                     </div>
                     <div className="middle-buttons">{createPostButton}</div>
                     <div className="buttons buttons-right">
-                        {profileButton}
-                        {button1}
-                        {button2}
+                        {userButtons}
+                        {authButtons}
                     </div>
                 </div>
             </div>
